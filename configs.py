520d010b98a55ceb07a3cfc9d850f01cdc7f0c6c
Ellie <csyccya@hotmail.com>
2021-04-12 20:42:03 -0700

    update config

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 HOST = "localhost"
 USER = "root"
-PASSWORD = "password" # feel your own password
+PASSWORD = "" # feel your own password
 DATABASE = "exchange_api"
 TABLE = "index"
 SERVER_HOST = "localhost"
